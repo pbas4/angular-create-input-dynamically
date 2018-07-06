d881c240623d3186cd303c7fbaeb3165d6cb55f5
Pol Bas Mantilla <pbas@zemsania.com>
2018-07-06 12:02:36 +0200

    Import correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
