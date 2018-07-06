093a119413a93da12b8adae59b0ae45a846810ce
Pol Bas Mantilla <pbas@zemsania.com>
2018-07-06 11:53:17 +0200

    Ready

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  nameForm: FormGroup;
+  items: FormArray;
+
+  constructor(public fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.nameForm = this.fb.group({
+      items: this.fb.array([ this.createItem() ])
+    });
+  }
+
+  createItem() {
+    return this.fb.group({
+      label: ['', Validators.required],
+      value: ['', Validators.required]
+    });
+  }
+
+  addItem() {
+    this.items = this.nameForm.get('items') as FormArray;
+    this.items.push(this.createItem());
+  }
+
+  getItems() {
+    console.log(this.nameForm.get('items')['controls']);
+  }
+
+  getItemsValues() {
+    const formGroups = this.nameForm.get('items')['controls'];
+    const valuesArray = [];
+
+    for (const group of formGroups) {
+      valuesArray.push(group.value);
+    }
+
+    console.log('valuesArray', valuesArray);
+  }
+
+}
